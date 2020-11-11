17b4610f36631c8796c5449e95c96c39532bd1a2
wiflore <williamflorezmaestre@gmail.com>
2020-11-11 00:14:54 -0500

    Ajax and error handling

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,11 +17,25 @@ db.create_all() #sync database
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.form.get('description', '')
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index'))
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+    if error:
+      abort(400)
+    else:
+      return jsonify(body)
+
 
 @app.route('/')
 def index():
