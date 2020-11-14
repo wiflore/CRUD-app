c0800234b8e493031d771150607d2965f767b4cb
wiflore <williamflorezmaestre@gmail.com>
2020-11-13 22:17:52 -0500

    update feature

--- todoapp/app.py
+++ todoapp/app.py
@@ -21,10 +21,13 @@ def create_todo():
   body = {}
   try:
     description = request.get_json()['description']
-    todo = Todo(description=description)
+    todo = Todo(description=description, completed=False)
     db.session.add(todo)
     db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
     body['description'] = todo.description
+    
   except:
     error = True
     db.session.rollback()
@@ -36,7 +39,27 @@ def create_todo():
     else:
       return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    print('todo', todo)
+    todo.completed = completed
+    print('todo.completed', todo.completed)
+
+    
+    db.session.commit()
+    print('todo', todo)
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
+
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', data=Todo.query.order_by('id').all())
