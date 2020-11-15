d1393139cbf1a8b9c661631ab65b5cc93ab9e692
wiflore <williamflorezmaestre@gmail.com>
2020-11-14 22:38:18 -0500

    app with Delete item

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,20 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+# sudo -u postgres -i
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp2'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+#migrate = Migrate(app, db)
 
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False)
 
-  def __repr__(self): #for debug proporse
-    return f'<Todo {self.id} {self.description}'
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
 
 db.create_all() #sync database
 
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
+
+# note: more conventionally, we would write a
+# POST endpoint to /todos for the create endpoint:
+# @app.route('/todos', method=['POST'])
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
   error = False
@@ -27,17 +49,16 @@ def create_todo():
     body['id'] = todo.id
     body['completed'] = todo.completed
     body['description'] = todo.description
-    
   except:
     error = True
     db.session.rollback()
     print(sys.exc_info())
   finally:
     db.session.close()
-    if error:
-      abort(400)
-    else:
-      return jsonify(body)
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
@@ -45,21 +66,14 @@ def set_completed_todo(todo_id):
     completed = request.get_json()['completed']
     print('completed', completed)
     todo = Todo.query.get(todo_id)
-    print('todo', todo)
     todo.completed = completed
-    print('todo.completed', todo.completed)
-
-    
     db.session.commit()
-    print('todo', todo)
   except:
     db.session.rollback()
   finally:
     db.session.close()
   return redirect(url_for('index'))
 
-
-
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.order_by('id').all())
+  return render_template('index.html', todos=Todo.query.order_by('id').all())
